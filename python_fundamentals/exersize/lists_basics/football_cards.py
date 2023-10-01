5bc9a98470c89e2dbb8941d56583b72f9ac5b225
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-10-01 11:02:11 +0300

    Create football_cards.py

---
+++ python_fundamentals/exersize/lists_basics/football_cards.py
@@ -0,0 +1,15 @@
+team_a = ['A-' + str(s) for s in range(1,12)]   # number of football players
+team_b = ['B-' + str(s) for s in range(1,12)]   # number of football players
+players = input().split()
+game_was_terminated = False
+for player in players:
+    if player in team_a:
+        team_a.remove(player)
+    elif player in team_b:
+        team_b.remove(player)
+    if len(team_a) < 7 or len(team_b) < 7:
+        game_was_terminated = True
+        break
+print(f"Team A - {len(team_a)}; Team B - {len(team_b)}")
+if game_was_terminated:
+    print('Game was terminated')
