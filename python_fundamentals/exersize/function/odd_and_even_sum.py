63e2d088d67294c17f42461a4ef7ed9dc1f845f5
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-10-06 20:40:21 +0300

    Create odd_and_even_sum.py

---
+++ python_fundamentals/exersize/function/odd_and_even_sum.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+
+def even_odd_sums(some_number : str) -> tuple[int , int]:
+    sum_of_even = 0
+    sum_of_odd = 0
+    for digit in some_number:
+        if int(digit) % 2 == 0:
+            sum_of_even += int(digit)
+        else:
+            sum_of_odd += int(digit)
+    return sum_of_odd, sum_of_even
+
+
+number = input()
+sum_of_odd_digits, sum_of_even_digits = even_odd_sums(number)
+print(f'Odd sum = {sum_of_odd_digits}, Even sum = {sum_of_even_digits}')
