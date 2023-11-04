4df065d6a79437e82e4bed8f99f71699ed446100
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-04 13:14:32 +0200

    Create student_academy.py

---
+++ python_fundamentals/exersize/dictionaries/student_academy.py
@@ -0,0 +1,13 @@
+student_grade_counts = int(input())
+student_grades_dict = {}
+for count in range(student_grade_counts):
+    student = input()
+    grade = float(input())
+    if student not in student_grades_dict:
+        student_grades_dict[student] = []
+    student_grades_dict[student].append(grade)
+
+for student , grades in student_grades_dict.items():
+    average_grade = sum(grades) / len(grades)
+    if average_grade >= 4.50:
+        print(f"{student} -> {average_grade:.2f}")
