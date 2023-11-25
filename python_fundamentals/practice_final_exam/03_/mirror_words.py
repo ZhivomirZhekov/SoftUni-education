96c1c4d8490139877fd8bb0e58b92536ff7f9933
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-25 12:36:45 +0200

    Update mirror_words.py

--- python_fundamentals/practice_final_exam/03_/mirror_words.py
+++ python_fundamentals/practice_final_exam/03_/mirror_words.py
@@ -5,7 +5,6 @@ pattern = r'(#|@)([A-Za-z]{3,})(\1{2})([A-Za-z]{3,})\1'
 result = re.findall(pattern , data)
 mirror_word = []
 words_count = len(result)
-
 for pair in result:
     if pair[1] == pair[3][::-1]:
         mirror_word.append(f'{pair[1]} <=> {pair[3]}')
