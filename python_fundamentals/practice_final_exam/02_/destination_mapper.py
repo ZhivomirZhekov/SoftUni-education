ebf53a021e62240293bbcb737a6fa483656db61b
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-25 13:58:34 +0200

    Create destination_mapper.py

---
+++ python_fundamentals/practice_final_exam/02_/destination_mapper.py
@@ -0,0 +1,15 @@
+import re
+
+string = input()
+valid_locations = []
+travel_points = 0
+pattern = r'(=|\/)([A-Z][A-Za-z]{3,})\1'
+matches = re.findall(pattern , string)
+if matches:
+    for match in matches:
+        location = match[1]
+        valid_locations.append(location)
+        travel_points += len(location)
+print(f"Destinations: {', '.join(valid_locations)}")
+print(f"Travel Points: {travel_points}")
+
