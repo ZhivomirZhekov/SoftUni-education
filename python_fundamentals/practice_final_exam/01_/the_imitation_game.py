c3ec313a9bc860698a0cf358a9f40c118f8e15f2
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-27 19:55:22 +0200

    Update the_imitation_game.py

--- python_fundamentals/practice_final_exam/01_/the_imitation_game.py
+++ python_fundamentals/practice_final_exam/01_/the_imitation_game.py
@@ -1,37 +1,34 @@
-def move(message: str , letters: int) -> str:
-    left_message = message[:letters]
-    right_message = message[letters:]
-    message = right_message + left_message
-    return message
+def move(encrypted_message , number_of_letters):
+    encrypted_message = encrypted_message[number_of_letters:] + encrypted_message[:number_of_letters]
+    return encrypted_message
 
 
-def insert(message: str , position_index: int , value_char: str) -> str:
-    left_message = message[:position_index]
-    right_message = message[position_index:]
-    message = left_message + value_char + right_message
-    return message
+def insert(encrypted_message , index , value):
+    encrypted_message = encrypted_message[:index] + value + encrypted_message[index:]
+    return encrypted_message
 
 
-def change_all(message: str , new_string: str , old_string: str) -> str:
-    message = message.replace(old_string , new_string)
-    return message
+def change_all(encrypted_message , substring , replacement):
+    encrypted_message = encrypted_message.replace(substring , replacement)
+    return encrypted_message
 
 
-encrypted_message = input()
+def main_function():
+    encrypted_message = input()
+    while True:
+        command = input().split("|")
+        if command[0] == "Decode":
+            print(f"The decrypted message is: {encrypted_message}")
+            break
+        if command[0] == "Move":
+            number_of_letters = int(command[1])
+            encrypted_message = move(encrypted_message , number_of_letters)
+        elif command[0] == "Insert":
+            index , value = int(command[1]) , command[2]
+            encrypted_message = insert(encrypted_message , index , value)
+        elif command[0] == "ChangeAll":
+            substring , replacement = command[1] , command[2]
+            encrypted_message = change_all(encrypted_message , substring , replacement)
 
-while True:
-    command = input().split("|")
-    if command[0] == 'Decode':
-        break
-    if command[0] == "Move":
-        number_of_letter = int(command[1])
-        encrypted_message = move(encrypted_message , number_of_letter)
-    elif command[0] == "Insert":
-        index = int(command[1])
-        value = command[2]
-        encrypted_message = insert(encrypted_message , index , value)
-    elif command[0] == "ChangeAll":
-        substring = command[1]
-        replacement = command[2]
-        encrypted_message = change_all(encrypted_message , replacement , substring)
-print(f"The decrypted message is: {encrypted_message}")
+
+main_function()
