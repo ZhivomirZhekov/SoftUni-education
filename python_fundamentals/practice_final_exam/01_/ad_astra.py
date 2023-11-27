bb854b8371c8ea906e9ed30139b75acc6354b28d
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-27 20:19:20 +0200

    Update ad_astra.py

--- python_fundamentals/practice_final_exam/01_/ad_astra.py
+++ python_fundamentals/practice_final_exam/01_/ad_astra.py
@@ -12,3 +12,17 @@ for element in matches:
     expiration_date = element[3]
     calories = element[5]
     print(f"Item: {item_name}, Best before: {expiration_date}, Nutrition: {calories}")
+# import re
+#
+# text_input = input()
+# pattern = r"(#|\|)([A-Za-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1"
+# matches = re.findall(pattern , text_input)
+# total_days = 0
+# for match in matches:
+#     calories = int(match[3])
+#     total_days += calories
+# total_days = total_days // 2000
+# print(f"You have food to last you for: {total_days} days!")
+# for match in matches:
+#     item_name , expiration_date , calories = match[1] , match[2] , int(match[3])
+#     print(f"Item: {item_name}, Best before: {expiration_date}, Nutrition: {calories}")
