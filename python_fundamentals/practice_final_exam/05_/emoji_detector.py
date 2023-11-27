465ac2ed51e49b0ee3878b37cdd3ba4366cbf54c
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-27 19:32:41 +0200

    Update emoji_detector.py

--- python_fundamentals/practice_final_exam/05_/emoji_detector.py
+++ python_fundamentals/practice_final_exam/05_/emoji_detector.py
@@ -3,24 +3,32 @@ import re
 input_text = input()
 
 numbers_match = re.findall(r'\d' , input_text)
-cool_threshold = 0
-if numbers_match:
-    cool_threshold = 1
-    for number in numbers_match:
-        cool_threshold *= int(number)
+cool_threshold = 1
+for number in numbers_match:
+    cool_threshold *= int(number)
 
-pattern = r'([:|*]{2})([A-Z][a-z]{2,})(\1)'
-matches = re.findall(pattern , input_text)
-print(f"Cool threshold: {cool_threshold}")
-emoji_count = len(matches)
-print(f"{emoji_count} emojis found in the text. The cool ones are:")
+pattern = r'(:{2}|\*{2})([A-Z][a-z]{2,})(\1)'
+
+matches = re.finditer(pattern , input_text)
+emoji_count = 0
+cool_emoji = []
 for match in matches:
-    emoji = match[1]
+    emoji_count += 1
+    emoji = match.group()
     emoji_coolness = 0
     for index in range(len(emoji)):
-        emoji_coolness += ord(emoji[index])
+        if emoji[index].isalpha():
+            emoji_coolness += ord(emoji[index])
     if emoji_coolness >= cool_threshold:
-        print(''.join(match))
+        cool_emoji.append(emoji)
+print(f"Cool threshold: {cool_threshold}")
+print(f"{emoji_count} emojis found in the text. The cool ones are:")
+for emoji in cool_emoji:
+    print(emoji)
+# # ['3', '1', '1', '3', '1', '1', '2', '3', '5', '2', '1']
+# # [('::', 'Smiley', '::'), ('**', 'Tigers', '**'), ('::', 'Mooning', '::'), ('**', 'Shy', '**')]
+# <re.Match object; span=(49, 59), match='::Smiley::'>
+# <re.Match object; span=(76, 86), match='**Tigers**'>
+# <re.Match object; span=(185, 196), match='::Mooning::'>
+# <re.Match object; span=(197, 204), match='**Shy**'>
 
-# ['3', '1', '1', '3', '1', '1', '2', '3', '5', '2', '1']
-# [('::', 'Smiley', '::'), ('**', 'Tigers', '**'), ('::', 'Mooning', '::'), ('**', 'Shy', '**')]
