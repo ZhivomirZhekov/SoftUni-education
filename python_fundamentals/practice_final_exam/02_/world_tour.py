d707d3325c94a28143ad173d3c226685705cc859
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-11-30 20:02:59 +0200

    Update world_tour.py

--- python_fundamentals/practice_final_exam/02_/world_tour.py
+++ python_fundamentals/practice_final_exam/02_/world_tour.py
@@ -1,43 +1,46 @@
-def add_stop(string: str , index_to_add: int , string_to_add: str) -> str:
-    if 0 <= index_to_add < len(string):
-        left_part = string[:index_to_add]
-        right_part = string[index_to_add:]
-        string = left_part + string_to_add + right_part
-    return string
+def add_stop(travel_stops , index , string):
+    if 0 < index < len(travel_stops):
+        travel_stops = travel_stops[:index] + string + travel_stops[index:]
+    return travel_stops
 
 
-def remove_stop(string: str , from_index: int , to_index: int) -> str:
-    if 0 <= from_index < len(string) and 0 <= to_index < len(string):
-        left_part = string[:from_index]
-        right_part = string[to_index + 1:]
-        string = left_part + right_part
-    return string
+def remove_stop(travel_stops , start_index , end_index):
+    if 0 < start_index < len(travel_stops) and 0 < end_index < len(travel_stops):
+        string_to_remove = travel_stops[start_index:end_index + 1]
+        travel_stops = travel_stops.replace(string_to_remove , "")
+    return travel_stops
 
 
-def switch(string: str , old_string: str , new_string: str) -> str:
-    if old_string in string:
-        string = string.replace(old_string, new_string)
-    return string
+def switch(travel_stops , old_string , new_string):
+    if old_string in travel_stops:
+        travel_stops = travel_stops.replace(old_string , new_string)
+    return travel_stops
 
 
-tour_stops = input()
-while True:
-    command = input().split(":")
-    if command[0] == "Travel":
-        break
-    if command[0] == "Add Stop":
-        index = int(command[1])
-        string = command[2]
-        tour_stops = add_stop(tour_stops , index , string)
-        print(tour_stops)
-    elif command[0] == "Remove Stop":
-        start_index = int(command[1])
-        end_index = int(command[2])
-        tour_stops = remove_stop(tour_stops , start_index , end_index)
-        print(tour_stops)
-    elif command[0] == "Switch":
-        old_string = command[1]
-        new_string = command[2]
-        tour_stops = switch(tour_stops , old_string , new_string)
-        print(tour_stops)
-print(f"Ready for world tour! Planned stops: {tour_stops}")
+def main_function():
+    travel_stops = input()
+    while True:
+        command = input()
+        if command == 'Travel':
+            break
+        command = command.split(":")
+        action = command[0]
+        if action == "Add Stop":
+            index = int(command[1])
+            string = command[2]
+            travel_stops = add_stop(travel_stops , index , string)
+            print(travel_stops)
+        elif action == "Remove Stop":
+            start_index = int(command[1])
+            end_index = int(command[2])
+            travel_stops = remove_stop(travel_stops , start_index , end_index)
+            print(travel_stops)
+        elif action == "Switch":
+            old_string = command[1]
+            new_string = command[2]
+            travel_stops = switch(travel_stops , old_string , new_string)
+            print(travel_stops)
+    print(f"Ready for world tour! Planned stops: {travel_stops}")
+
+
+main_function()
