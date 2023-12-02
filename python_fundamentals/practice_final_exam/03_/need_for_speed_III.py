933448cee953291b43cafa69bd31aea69977897d
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-12-02 11:27:56 +0200

    Update need_for_speed_III.py

--- python_fundamentals/practice_final_exam/03_/need_for_speed_III.py
+++ python_fundamentals/practice_final_exam/03_/need_for_speed_III.py
@@ -1,83 +1,76 @@
-def car_attributes() -> dict:
+def car_create(car_name , mileage , fuel):
+    return {"car_name": car_name , "mileage": mileage , "fuel": fuel}
+
+
+def drive(cars , car_name , distance , fuel):
+    for car in cars:
+        if car["car_name"] == car_name:
+            if car["fuel"] >= fuel:
+                car["fuel"] -= fuel
+                car["mileage"] += distance
+                print(f'{car["car_name"]} driven for {distance} kilometers. {fuel} liters of fuel consumed.')
+                if car["mileage"] >= 100000:
+                    print(f'Time to sell the {car["car_name"]}!')
+                    cars.remove(car)
+                return cars
+            else:
+                print("Not enough fuel to make that ride")
+                return cars
+
+
+
+def refuel(cars , max_fuel , car_name , fuel):
+    for car in cars:
+        if car['car_name'] == car_name:
+            before_refuel = car['fuel']
+            car['fuel'] = min(max_fuel , before_refuel + fuel)
+            refueled = car['fuel'] - before_refuel
+            print(f"{car['car_name']} refueled with {refueled} liters")
+            return cars
+
+
+def revert(cars , car_name , kilometers):
+    for car in cars:
+        if car['car_name'] == car_name:
+            car['mileage'] -= kilometers
+            if car['mileage'] < 10000:
+                car['mileage'] = 10000
+                return cars
+            else:
+                print(f"{car['car_name']} mileage decreased by {kilometers} kilometers")
+                return cars
+
+
+def main_function():
     number_of_cars = int(input())
-    cars_dict = {}
-    _car_mileage = "mileage"
-    _car_fuel = "fuel"
-    for number in range(number_of_cars):
-        car_mileage_fuel = input().split('|')
-        _car = car_mileage_fuel[0]
-        _mileage = int(car_mileage_fuel[1])
-        _fuel = int(car_mileage_fuel[2])
-        if _car not in cars_dict:
-            cars_dict[_car] = {}
-        cars_dict[_car][_car_mileage] = _mileage
-        cars_dict[_car][_car_fuel] = _fuel
-    return cars_dict
+    cars = []
+    for _ in range(number_of_cars):
+        car_name , mileage , fuel = input().split("|")
+        mileage = int(mileage)
+        fuel = int(fuel)
+        cars.append(car_create(car_name , mileage , fuel))
+    while True:
+        command = input().split(" : ")
+        action = command[0]
+        if action == "Stop":
+            break
+
+        if action == "Drive":
+            car_name = command[1]
+            distance = int(command[2])
+            fuel = int(command[3])
+            cars = drive(cars , car_name , distance , fuel)
+        elif action == "Refuel":
+            max_fuel = 75
+            car_name = command[1]
+            fuel = int(command[2])
+            cars = refuel(cars , max_fuel , car_name , fuel)
+        elif action == "Revert":
+            car_name = command[1]
+            kilometers = int(command[2])
+            cars = revert(cars , car_name , kilometers)
+    for car in cars:
+        print(f"{car['car_name']} -> Mileage: {car['mileage']} kms, Fuel in the tank: {car['fuel']} lt.")
 
 
-def drive(cars_dict: dict , _car: str , _distance: int , _fuel: int):
-    _car_mileage = "mileage"
-    _car_fuel = "fuel"
-    if _car not in cars_dict.keys():
-        return cars_dict
-    elif cars_dict[_car][_car_fuel] <= _fuel:
-        print("Not enough fuel to make that ride")
-        return cars_dict
-    else:
-        cars_dict[_car][_car_mileage] += _distance
-        cars_dict[_car][_car_fuel] -= _fuel
-        print(f"{_car} driven for {_distance} kilometers. {_fuel} liters of fuel consumed.")
-        if cars_dict[_car][_car_mileage] >= 100000:
-            del cars_dict[_car]
-            print(f"Time to sell the {_car}!")
-            return cars_dict
-
-        return cars_dict
-
-
-def refuel(cars_dict: dict , _car: str , _fuel: int):
-    tank_max_capacity = 75
-    _car_fuel = "fuel"
-    litters_to_fill = min(75 - cars_dict[_car][_car_fuel] , fuel)
-    cars_dict[_car][_car_fuel] = min(cars_dict[_car][_car_fuel] + _fuel , tank_max_capacity)
-
-    print(f"{_car} refueled with {litters_to_fill} liters")
-    return cars_dict
-
-
-def revert(cars_dict: dict , _car : str , kilometers: int):
-    _car_mileage = "mileage"
-    cars_dict[_car][_car_mileage] -= kilometers
-    if cars_dict[_car][_car_mileage] < 10000:
-        cars_dict[_car][_car_mileage] = 10000
-    else:
-        print(f"{_car} mileage decreased by {kilometers} kilometers")
-    return cars_dict
-
-
-cars = car_attributes()
-
-while True:
-    command = input().split(" : ")
-    if command[0] == "Stop":
-        break
-    if command[0] == "Drive":
-        car = command[1]
-        distance = int(command[2])
-        fuel = int(command[3])
-        cars = drive(cars , car , distance , fuel)
-    elif command[0] == "Refuel":
-        car = command[1]
-        fuel = int(command[2])
-        refuel(cars , car , fuel)
-    elif command[0] == "Revert":
-        car = command[1]
-        distance = int(command[2])
-        revert(cars , car , distance)
-
-for car , attributes in cars.items():
-    car_mileage = "mileage"
-    car_fuel = "fuel"
-    mileage = attributes[car_mileage]
-    fuel = attributes[car_fuel]
-    print(f"{car} -> Mileage: {mileage} kms, Fuel in the tank: {fuel} lt.")
+main_function()
