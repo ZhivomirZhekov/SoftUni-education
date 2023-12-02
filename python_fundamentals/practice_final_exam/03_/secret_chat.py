a67149188c7ce2bdc5584752b5cdb0e63ec6bc05
ZhivomirZhekov <145650351+ZhivomirZhekov@users.noreply.github.com>
2023-12-02 10:03:49 +0200

    Update secret_chat.py

--- python_fundamentals/practice_final_exam/03_/secret_chat.py
+++ python_fundamentals/practice_final_exam/03_/secret_chat.py
@@ -1,45 +1,47 @@
-def insert_space(string: str , _index: int) -> str:
-    left_side = string[:_index]
-    right_side = string[_index:]
-    string = left_side + " " + right_side
-    return string
+def insert_space(concealed_message: str , index):
+    if 0 <= index < len(concealed_message):
+        concealed_message = concealed_message[:index] + " " + concealed_message[index:]
+    return concealed_message
 
 
-def reverse(string: str , string_to_reverse: str , _is_reversed: bool):
-    if string_to_reverse in string:
-        reversed_string = string_to_reverse[::-1]
-        string = string.replace(string_to_reverse , "" , 1)
-        string = string + reversed_string
+def reverse(concealed_message , substring):
+    if substring in concealed_message:
+        reversed_substring = substring[::-1]
+        concealed_message = concealed_message.replace(substring , reversed_substring , 1)
+        print(concealed_message)
     else:
         print("error")
-        _is_reversed = False
-    return string , _is_reversed
+    return concealed_message
 
 
-def change_all(string: str , string_to_replace: str , replacement_string: str) -> str:
-    if string_to_replace in string:
-        string = string.replace(string_to_replace , replacement_string)
-    return string
+def change_all(concealed_message , substring , replacement):
+    if substring in concealed_message:
+        concealed_message = concealed_message.replace(substring , replacement)
+    return concealed_message
 
 
-concealed_message = input()
-while True:
-    instruction = input().split(":|:")
-    if instruction[0] == "Reveal":
-        break
-    if instruction[0] == "InsertSpace":
-        index = int(instruction[1])
-        concealed_message = insert_space(concealed_message , index)
+def main_function():
+    concealed_message = input()
+    while True:
+        command = input().split(":|:")
+        action = command[0]
+        if action == "Reveal":
+            break
 
-    elif instruction[0] == "Reverse":
-        is_reversed = True
-        substring = instruction[1]
-        concealed_message , is_reversed = reverse(concealed_message , substring , is_reversed)
-        if not is_reversed:
-            continue
-    elif instruction[0] == "ChangeAll":
-        substring = instruction[1]
-        replacement = instruction[2]
-        concealed_message = change_all(concealed_message , substring , replacement)
-    print(concealed_message)
-print(f"You have a new text message: {concealed_message}")
+        if action == "InsertSpace":
+            index = int(command[1])
+            concealed_message = insert_space(concealed_message , index)
+            print(concealed_message)
+        elif action == "Reverse":
+            substring = command[1]
+            concealed_message = reverse(concealed_message , substring)
+
+        elif action == "ChangeAll":
+            substring = command[1]
+            replacement = command[2]
+            concealed_message = change_all(concealed_message , substring , replacement)
+            print(concealed_message)
+    print(f"You have a new text message: {concealed_message}")
+
+
+main_function()
